8bd9ef3807e039560d979b1dff876a3114326002
Codi-chi <romvent20@gmail.com>
2026-05-14 22:41:18 -0400

    password normal

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bd_archivo',
         'USER': 'postgres',
-        'PASSWORD': 'comando123',
+        'PASSWORD': 'servidorFarmacia947',
         'HOST': 'localhost',
         'PORT': '5432',
     }
